7c9b2bdf906548d3fdce5b9fe2d837f8ccf993d9
ether99 <ether8@yandex.ru>
2022-11-30 02:32:27 +0300

    VKViewControl

---
+++ VK GU_IOS_2930/VKViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VKViewController.swift
+//  VK GU_IOS_2930
+//
+//  Created by Роберт on 30.11.2022.
+//
+
+import UIKit
+
+class VKViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
