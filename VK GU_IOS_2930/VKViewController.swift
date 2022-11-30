3ad2fcb3ed3160e0f980d0ecac01a929e75d055a
ether99 <ether8@yandex.ru>
2022-11-30 21:25:24 +0300

    VKViewController

--- VK GU_IOS_2930/VKViewController.swift
+++ VK GU_IOS_2930/VKViewController.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
-class VKViewController: UITabBarController {
-
+class VKViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(hideScreen))
+        
+        view.addGestureRecognizer(tapGR)
+        
+    }
+    @objc func hideScreen() {
+        view.endEditing(true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(willShowKeyboard), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(willHideKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-    */
-
+    @objc func willShowKeyboard(_ notification: Notification) {
+        print(#function)
+    }
+    
+    @objc func willHideKeyboard(_ notification: Notification) {
+        print(#function)
+    }
+    
 }
