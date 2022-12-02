991c641c9a15b1f951c47a6c5b4078ff77c64990
ether99 <ether8@yandex.ru>
2022-12-02 11:39:33 +0300

    secondLesson

--- VK GU_IOS_2930/VKViewController.swift
+++ VK GU_IOS_2930/VKViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class VKViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,10 +34,32 @@ class VKViewController: UIViewController {
     }
     @objc func willShowKeyboard(_ notification: Notification) {
         print(#function)
+        
+        guard let info = notification.userInfo as NSDictionary?,
+              let keyboardSize = info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as?
+                NSValue else {return}
+        let keyBoardHeight = keyboardSize.cgRectValue.size.height
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyBoardHeight, right: 0)
+        
     }
     
     @objc func willHideKeyboard(_ notification: Notification) {
         print(#function)
+        
+        scrollView.contentInset = .zero
     }
     
+    
+    
+    @IBAction func tapButtonLogin(_ sender: Any) {
+        
+        guard let login = loginTextField.text,
+              let password = passwordTextField.text,
+              login == "",
+        password == "" else {return}
+        
+        performSegue(withIdentifier: "Login", sender: nil)
+        
+    }
 }
+
